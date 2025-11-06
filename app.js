9d0d2179ce8f1c4c7f1bb633cea5b0437345a34a
TaniaRizwan <105510572+TaniaRizwan@users.noreply.github.com>
2025-11-06 00:14:31 -0700

    Implemented working landing page. About page in progress

---
+++ app.js
@@ -0,0 +1,10 @@
+
+document.getElementById("AboutBtn").addEventListener("click", () => {
+    window.location.href = "about.html"
+});
+
+
+document.getElementById("SignInBtn").addEventListener("click", () => {
+    window.location.href = "signin.html"
+});
+
